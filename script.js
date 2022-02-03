27b4cb7b86196c94ea38773fc7d95441d193f206
Atinos31 <atinosandra2014@gmail.com>
2022-02-03 20:52:34 +0000

    create and customise landing page

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 
-const slideshowDivs = () =>{
-    for(let i = 1; i <=5; i++){
-        const div = document.createElement('div')
+const slideshowDivs = () => {
+    for(let i = 1; i <= 5; i++){
+        const div = document.createElement('div');
 
         div.style.backgroundImage = `url(images/slidehow/section-1-bg-${i}.jpg)`
-        i === 1 && div.classList.add('change')
+        i === 1 && div.classList.add('change');
 
-        document.querySelector('.slideshow').appendChild(div)
-
+        document.querySelector('.slideshow').appendChild(div);
     }
 }
 
-slideshowDivs()
+slideshowDivs();
+
+const divs = document.querySelectorAll('.slideshow div');
+
+let a = 1
+
+const slideshow = () => {
+    setInterval(() => {
+        a++
+      const div = document.querySelector('.slideshow .change');
+
+      div.classList.remove('change')
+      if(a < divs.length){
+      div.nextElementSibling.classList.add('change')
+      } else{
+          divs[0].classList.add('change')
+          a = 1
+      }
+    }, 20000)
+}
+
+slideshow();
