7d86cc38104c79e4979faddf6e3f0a0e71c0aaee
Atinos31 <atinosandra2014@gmail.com>
2022-02-08 09:20:32 +0000

    create and customise apple box cube controllers

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 const slideshowDivs = () => {
     for(let i = 1; i <= 5; i++){
         const div = document.createElement('div');
-
         div.style.backgroundImage = `url(images/slideshow/section-1-bg-${i}.jpg)`
         i === 1 && div.classList.add('change');
 
@@ -11,16 +10,12 @@ const slideshowDivs = () => {
 }
 
 slideshowDivs();
-
 const divs = document.querySelectorAll('.slideshow div');
-
 let a = 1
-
 const slideshow = () => {
     setInterval(() => {
         a++
       const div = document.querySelector('.slideshow .change');
-
       div.classList.remove('change')
       if(a < divs.length){
       div.nextElementSibling.classList.add('change')
