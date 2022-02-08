6be907c85862c3f8031325bf381eca8fa5743a54
Atinos31 <atinosandra2014@gmail.com>
2022-02-08 21:52:59 +0000

    create and style macbook section part 2

--- script.js
+++ script.js
@@ -79,3 +79,16 @@ const slideshow = () => {
 }
 
 slideshow();
+// end slideshow
+
+// section3
+const section3Content = document.querySelector('.section-3-content')
+
+window.addEventListener('scroll', () => {
+    if(window.pageYOffset + window.innerHeight >= section3Content.offsetTop
+    + section3Content.offsetHeight /2){
+        section3Content.classList.add('change')
+
+    }
+})
+// end section3
