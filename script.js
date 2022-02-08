0febc337b25bf647bcd5a9ef00041896f073361b
Atinos31 <atinosandra2014@gmail.com>
2022-02-08 08:38:09 +0000

    fix typo bug

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-
+//slideshow
 const slideshowDivs = () => {
     for(let i = 1; i <= 5; i++){
         const div = document.createElement('div');
 
-        div.style.backgroundImage = `url(images/slidehow/section-1-bg-${i}.jpg)`
+        div.style.backgroundImage = `url(images/slideshow/section-1-bg-${i}.jpg)`
         i === 1 && div.classList.add('change');
 
         document.querySelector('.slideshow').appendChild(div);
